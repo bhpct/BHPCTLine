4f407deaa075154d4f352f22328d753fe0afed3e
bhpct <song@bhpct.tw>
2026-03-26 18:25:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // 若要將系統部署至其他教會，只需修改此檔案即可！
 // ==========================================
 
-const CONFIG = {
+window.CONFIG = {
     // 1. 教會/機構名稱 (將自動替換所有網頁的標題)
     CHURCH_NAME: "霧峰教會",
     
@@ -18,7 +18,7 @@ const CONFIG = {
 };
 
 // 為了向下相容原有的程式碼，將變數映射到全域 (請勿更動下方)
-const myLiffId = CONFIG.LIFF_ID;
-const GAS_URL = CONFIG.GAS_URL;
-const CACHE_TTL = CONFIG.CACHE_TTL;
-const CACHE_PREFIX = 'church_data_';
+window.myLiffId = window.CONFIG.LIFF_ID;
+window.GAS_URL = window.CONFIG.GAS_URL;
+window.CACHE_TTL = window.CONFIG.CACHE_TTL;
+window.CACHE_PREFIX = 'church_data_';
